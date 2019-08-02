81a37867d21dbbc7e62b9c9c83335f9ae7815b8d
Kyne <1>
2019-08-02 09:08:17 -0700

    Add support for event scenes

--- ClothingStateMenu/ClothingStateMenu.cs
+++ ClothingStateMenu/ClothingStateMenu.cs
@@ -215,30 +215,40 @@ namespace KK_ClothingStateMenu
 
             var hFlag = FindObjectOfType<HFlag>();
             if (hFlag != null) _chaCtrl = hFlag.player.chaCtrl;
-            else _chaCtrl = GetCurrentVisibleGirl()?.chaCtrl;
+            else _chaCtrl = GetCurrentVisibleGirl();
         }
 
-        private static SaveData.Heroine GetCurrentVisibleGirl()
+        private static ChaControl GetCurrentVisibleGirl()
         {
             var result = FindObjectOfType<TalkScene>()?.targetHeroine;
             if (result != null)
-                return result;
+                return result.chaCtrl;
 
             var advScene = Manager.Game.Instance?.actScene?.AdvScene;
 
             if (advScene != null)
             {
-                if (advScene.Scenario?.currentHeroine != null) return advScene.Scenario.currentHeroine;
+                if (advScene.Scenario?.currentHeroine != null) return advScene.Scenario.currentHeroine.chaCtrl;
 
                 try
                 {
                     var advSceneTargetHeroineProp = typeof(ADV.ADVScene).GetField("m_TargetHeroine", BindingFlags.Instance | BindingFlags.NonPublic);
-                    return advSceneTargetHeroineProp?.GetValue(advScene.nowScene) as SaveData.Heroine;
+                    return (advSceneTargetHeroineProp?.GetValue(advScene.nowScene) as SaveData.Heroine).chaCtrl;
                 }
                 catch
                 {
                 }
             }
+
+            // In event
+            var character = Manager.Character.Instance;
+            if (character != null && character.dictEntryChara.Count > 0)
+            {
+                // Main event char is usually (not always) the first one.
+                // Also will pull the currently visible character, like the teacher or the mom.
+                return character.dictEntryChara[0];
+            }
+
             return null;
         }
 
