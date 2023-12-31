e7e399eaa728b6aa1f92b7ad6aa833c0d90e0b08
Starstorm <49352148+starstormhun@users.noreply.github.com>
2023-12-31 21:06:39 +0100

    Got rid of toggle memory, implemented proper toggle handling (#11)

---
+++ Shared/HarmonyPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using ChaCustom;
+
+namespace ClothingStateMenu {
+    public static class HarmonyPatches {
+        internal static void Init() {
+            Hooks.SetupHooks();
+        }
+
+        private static class Hooks {
+            public static void SetupHooks() {
+                Harmony.CreateAndPatchAll(typeof(Hooks), null);
+            }
+
+            // Disables aggressive syncing of the accessory toggle states to the vanilla buttons
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(CvsDrawCtrl), "UpdateAccessoryDraw")]
+            private static bool CvsDrawDisableUpdateAccessoryDraw() {
+                return false;
+            }
+        }    
+    }
+}
