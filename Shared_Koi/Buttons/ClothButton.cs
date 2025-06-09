c7d7ec3f814d5a990b21673d690b87d2370970b4
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-06-09 23:42:24 +0200

    Show "None" if a clothing piece is not equipped (#20)

--- Shared_Koi/Buttons/ClothButton.cs
+++ Shared_Koi/Buttons/ClothButton.cs
@@ -33,12 +33,33 @@ namespace ClothingStateMenu
         };
 
         private readonly GUIContent[] _contentStates;
+        private readonly GUIContent _contentEmpty;
 
         public readonly ChaFileDefine.ClothesKind Kind;
 
         private readonly ChaControl _chaCtrl;
 
-        public GUIContent Content => _contentStates[GetState()];
+        public GUIContent Content => IsVisible() ? _contentStates[GetState()] : _contentEmpty;
+
+        private bool IsVisible()
+        {
+            if (_chaCtrl.cusClothesCmp[(int)Kind])
+                return true;
+
+            switch (Kind)
+            {
+                case ChaFileDefine.ClothesKind.top:
+                    return _chaCtrl.cusClothesSubCmp[0] || _chaCtrl.cusClothesSubCmp[1] || _chaCtrl.cusClothesSubCmp[2];
+                case ChaFileDefine.ClothesKind.bot:
+                    return _chaCtrl.notBot;
+                case ChaFileDefine.ClothesKind.bra:
+                    return _chaCtrl.notBra;
+                case ChaFileDefine.ClothesKind.shorts:
+                    return _chaCtrl.notShorts;
+                default:
+                    return false;
+            }
+        }
 
         public void OnClick()
         {
@@ -57,6 +78,7 @@ namespace ClothingStateMenu
 
             var fancyName = _FancyKindNames[kind];
             _contentStates = _FancyStateNames.Select(fancyState => new GUIContent($"{fancyName}: {fancyState}")).ToArray();
+            _contentEmpty = new GUIContent(fancyName + ": None");
         }
     }
 }
